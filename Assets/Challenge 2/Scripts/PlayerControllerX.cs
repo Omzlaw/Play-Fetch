45b5dfafed48b91be2d64c1e6ab06bff05dffaef
Omzlaw <omeizaalabi@gmail.com>
2022-09-17 22:17:19 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float dogSpawnInterval = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if (dogSpawnInterval == 0)
+            // {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogSpawnInterval = 2.0f;
+            // }
+        }
+        // if (dogSpawnInterval != 0)
+        // {
+        //     dogSpawnInterval -= Time.deltaTime;
+        // }
+    }
+}
